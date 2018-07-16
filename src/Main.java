ea064810685a8aeeca5b1ad0f33ad85059365d2d
Ernest Xu <ernest.xu1024@gmail.com>
2018-07-16 20:15:20 +0800

    Refactor Root Directory

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-	    System.out.println("Use Hadoop MapReduce framework to implement auto complete, PageRank algorithm, and recommender system");
-    }
-}
