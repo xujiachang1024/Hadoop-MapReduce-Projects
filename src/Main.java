743082a15615cde237ed8d84f88abc94bbd9f5b8
Ernest Xu <ernest.xu1024@gmail.com>
2018-07-16 19:43:14 +0800

    Hadoop MapReduce Projects
    Use Hadoop MapReduce framework to implement auto complete, PageRank algorithm, and recommender system

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Use Hadoop MapReduce framework to implement auto complete, PageRank algorithm, and recommender system");
+    }
+}
