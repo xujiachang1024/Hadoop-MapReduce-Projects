a70fceb57aa8bdff57647c0d60a3765f488faa69
Ernest Xu <ernest.xu1024@gmail.com>
2018-07-17 10:07:07 +0800

    Recommender-System: Start Project

---
+++ MapReduce-Recommender-System/src/main/java/CoOccurrenceMatrixGenerator.java
@@ -0,0 +1,58 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+public class CoOccurrenceMatrixGenerator {
+	public static class MatrixGeneratorMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+		// map method
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			//value = userid \t movie1: rating, movie2: rating...
+			//key = movie1: movie2 value = 1
+			//calculate each user rating list: <movieA, movieB>
+			
+		}
+	}
+
+	public static class MatrixGeneratorReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		// reduce method
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			//key movie1:movie2 value = iterable<1, 1, 1>
+			//calculate each two movies have been watched by how many people
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		
+		Configuration conf = new Configuration();
+		
+		Job job = Job.getInstance(conf);
+		job.setMapperClass(MatrixGeneratorMapper.class);
+		job.setReducerClass(MatrixGeneratorReducer.class);
+		
+		job.setJarByClass(CoOccurrenceMatrixGenerator.class);
+		
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		TextInputFormat.setInputPaths(job, new Path(args[0]));
+		TextOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.waitForCompletion(true);
+		
+	}
+}
