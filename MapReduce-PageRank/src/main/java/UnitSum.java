673bc76728eb7723ad21ccea04ff1e83aa346562
Ernest Xu <ernest.xu1024@gmail.com>
2018-07-17 11:44:41 +0800

    PageRank: Finish UnitSum

--- MapReduce-PageRank/src/main/java/UnitSum.java
+++ MapReduce-PageRank/src/main/java/UnitSum.java
@@ -17,19 +17,54 @@ public class UnitSum {
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-            //input format: toPage\t unitMultiplication
-            //target: pass to reducer
+            // input format: toPage\t unitMultiplication
+            String[] toPage_unitMultiplication = value.toString().split("\t");
+            String toPage = toPage_unitMultiplication[0];
+            double unitMultiplication = Double.parseDouble(toPage_unitMultiplication[1]);
+
+            // target: pass to reducer
+            context.write(new Text(toPage), new DoubleWritable(unitMultiplication));
+        }
+    }
+
+    public static class BetaMapper extends Mapper<Object, Text, Text, DoubleWritable> {
+
+        float beta;
+
+        @Override
+        public void setup(Context context) {
+            Configuration configuration = context.getConfiguration();
+            beta = configuration.getFloat("beat", 0.2f);
+        }
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+            // input format: toPage\t pageRank
+            String[] toPage_pageRank = value.toString().split("\t");
+
+            // target: key=toPage, value=pageRank*beta
+            String toPage = toPage_pageRank[0];
+            double betaRank = Double.parseDouble(toPage_pageRank[1]) * beta;
+            context.write(new Text(toPage), new DoubleWritable(betaRank));
         }
     }
 
     public static class SumReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
 
         @Override
-        public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
-                throws IOException, InterruptedException {
+        public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
 
-           //input key = toPage value = <unitMultiplication>
-            //target: sum!
+            // input: key = toPage, value = <unitMultiplication>
+            double sum = 0;
+            for (DoubleWritable value : values) {
+                sum += value.get();
+            }
+
+            // target: sum
+            DecimalFormat decimalFormat = new DecimalFormat("#.0000");
+            sum = Double.valueOf(decimalFormat.format(sum));
+            context.write(key, new DoubleWritable(sum));
         }
     }
 
